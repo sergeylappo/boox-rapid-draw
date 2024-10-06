b2cb15d3afee10c22e6e1222f7272950386c266d
Sergey Lappo <sergeylappo@gmail.com>
2024-10-07 01:20:05 +0200

    Make version name reflect reality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1-alpha"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
