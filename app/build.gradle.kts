8d383e793aec9946858f69ad01d6d973da06b091
Sergey Lappo <sergeylappo@gmail.com>
2026-08-10 22:19:22 +0200

    Prepare release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.sergeylappo.booxrapiddraw"
         minSdk = 28
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.0.6-alpha"
+        versionCode = 7
+        versionName = "0.0.7-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
